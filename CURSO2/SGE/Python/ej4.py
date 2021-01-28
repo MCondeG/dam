4b18473ab3dd382fd041115045d64190dfa0acaa
MCondeG <mrcondegonz@gmail.com>
2021-01-28 01:37:56 +0100

    INICIO

---
+++ CURSO2/SGE/Python/ej4.py
@@ -0,0 +1,11 @@
+l1 = (float)(input('Introduce lado 1: '))
+l2 = (float)(input('Introduce lado 2: '))
+l3 = (float)(input('Introduce lado 3: '))
+
+if (l1 == l2 or l1 == l3 or l2 == l3):
+    if (l1 == l2 and l2 == l3):
+        print('Equilátero')
+    else:
+        print ('Isósceles')
+else:
+    print('Escaleno')
