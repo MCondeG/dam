4430d9012b097268c5fe582b2837383dd154f5eb
MCondeG <mrcondegonz@gmail.com>
2021-02-25 14:41:09 +0100

    25/02/21

--- CURSO2/SGE/Python/Evaluacion/Biblioteca.py
+++ CURSO2/SGE/Python/Evaluacion/Biblioteca.py
@@ -13,16 +13,51 @@ class Biblioteca:
         
         self.__tamañoActual = 5
         
-        self.__nombre[0] = ("El Señor de los Anillos")
-        self.__nombre[1] = ("Harry Potter y la Piedra Filosofal")
-        self.__nombre[2] = ("Los Pilares de la Tierra")
-        self.__nombre[3] = ("Canción de Hielo y Fuego")
-        self.__nombre[4] = ("El Quijote")
+        self.__nombre.append("El Señor de los Anillos")
+        self.__nombre.append("Harry Potter y la Piedra Filosofal")
+        self.__nombre.append("Los Pilares de la Tierra")
+        self.__nombre.append("Canción de Hielo y Fuego")
+        self.__nombre.append("El Quijote")
         
-        for i in range(0, self.__TAMAÑOMAX):
-            self.__estado[i] = random.randint(-1, 1)
-            self.__prestado[i] = random.randint(0, 10)
+        for i in range(0, self.__tamañoActual):
+            self.__estado.append(int(random.randint(-1, 1)))
+            self.__prestado.append(int(random.randint(0, 10)))
     
     
     def __init__(self):
-        self.iniciar()
+        self.iniciar(5)
+        
+    
+    def getNombre(self):
+        return self.__nombre
+    
+    def getEstado(self):
+        return self.__estado
+    
+    def getPrestado(self):
+        return self.__prestado
+    
+    
+    def buscaLibro(self, Nombre):
+        
+        for i in range(0, self.__tamañoActual):
+            if (Nombre == self.__nombre[i]):
+                
+    
+    def compraLibro(self, Nombre):
+        
+        if (self.__tamañoActual < self.__TAMAÑOMAX):
+            self.__nombre.append(Nombre)
+            self.__estado.append(int(0))
+            self.__prestado.append(int(0))
+            self.__tamañoActual += 1
+        else:
+            print("ERROR. Biblioteca completa")
+            
+    def tiraLibro(self, Nombre):
+        
+    def sacaLibro(self, Nombre):
+        
+    def devuelveLibro(self, Nombre):
+        
+    def libroMasPrestado(self):
---
+++ CURSO2/SGE/Python/Evaluacion/Main2.py
@@ -0,0 +1,5 @@
+from Biblioteca import *
+
+x = Biblioteca()
+
+print(x.getNombre())
