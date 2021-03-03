fdcdfeba26bdebdae31470be79c85bac1742146d
MCondeG <mrcondegonz@gmail.com>
2021-03-03 02:10:44 +0100

    03/03/21

--- CURSO2/SGE/Python/Evaluacion/Biblioteca.py
+++ CURSO2/SGE/Python/Evaluacion/Biblioteca.py
@@ -9,55 +9,119 @@ class Biblioteca:
     __tamañoActual = 0
     
     
-    def iniciar(self):
+    def iniciar(self, tamaño):
         
-        self.__tamañoActual = 5
+        self.__tamañoActual = tamaño
         
         self.__nombre.append("El Señor de los Anillos")
         self.__nombre.append("Harry Potter y la Piedra Filosofal")
         self.__nombre.append("Los Pilares de la Tierra")
-        self.__nombre.append("Canción de Hielo y Fuego")
-        self.__nombre.append("El Quijote")
+        self.__nombre.append("")
+        self.__nombre.append("")
         
         for i in range(0, self.__tamañoActual):
-            self.__estado.append(int(random.randint(-1, 1)))
+            self.__estado.append(int(random.randint(0, 1)))
             self.__prestado.append(int(random.randint(0, 10)))
-    
-    
-    def __init__(self):
-        self.iniciar(5)
+            
+        self.__estado.append(int(-1))
+        self.__estado.append(int(-1))
+        self.__prestado.append(int(0))
+        self.__prestado.append(int(0))
         
     
-    def getNombre(self):
-        return self.__nombre
+    def getNombre(self, pos):
+        return (self.__nombre[int(pos)])
     
-    def getEstado(self):
-        return self.__estado
+    def getEstado(self, pos):
+        return (self.__estado[int(pos)])
     
-    def getPrestado(self):
-        return self.__prestado
+    def getPrestado(self, pos):
+        return (self.__prestado[int(pos)])
+    
+    
+    def muestraBiblioteca(self):
+        
+        for i in range(0, self.__TAMAÑOMAX):
+            print(self.__nombre[i], ", Estado actual:", self.__estado[i], ", Prestado", self.__prestado[i], "veces")
     
     
     def buscaLibro(self, Nombre):
         
-        for i in range(0, self.__tamañoActual):
-            if (Nombre == self.__nombre[i]):
+        pos = -1
+        
+        for i in range(0, self.__TAMAÑOMAX):
+            if ((self.__estado[i] != -1) and (Nombre == self.__nombre[i])):
+                pos = i
+                    
+        return pos
                 
     
     def compraLibro(self, Nombre):
         
         if (self.__tamañoActual < self.__TAMAÑOMAX):
-            self.__nombre.append(Nombre)
-            self.__estado.append(int(0))
-            self.__prestado.append(int(0))
-            self.__tamañoActual += 1
+            for i in range(0, self.__TAMAÑOMAX):
+                if (self.__nombre[i] == ""):
+                    self.__nombre[i] = Nombre
+                    self.__estado[i] = 0
+                    self.__prestado[i] = 0
+                    self.__tamañoActual += 1
+                    print(Nombre,": Libro comprado con éxito")
+                    break
         else:
             print("ERROR. Biblioteca completa")
             
+            
     def tiraLibro(self, Nombre):
+        
+        pos = int(self.buscaLibro(Nombre))
+        
+        if (pos != -1):
+            print(self.__nombre[pos],": Libro tirado")
+            self.__nombre[pos] = ""
+            self.__estado[pos] = -1
+            self.__prestado[pos] = 0
+            self.__tamañoActual -= 1
+        else:
+            print("ERROR. Nombre de libro incorrecto")
+        
         
     def sacaLibro(self, Nombre):
         
+        pos = int(self.buscaLibro(Nombre))
+        
+        if (pos != -1):
+            if (self.__estado[pos] == 0):
+                self.__estado[pos] = 1
+                print(self.__nombre[pos],": Libro prestado con éxito")
+            else:
+                print("ERROR. Libro actualmente en préstamo")
+        else:
+            print("ERROR. Nombre de libro incorrecto")
+        
+        
     def devuelveLibro(self, Nombre):
         
+        pos = int(self.buscaLibro(Nombre))
+        
+        if (pos != -1):
+            if (self.__estado[pos] == 1):
+                self.__estado[pos] = 0
+                self.__prestado[pos] += 1
+                print(self.__nombre[pos],": Libro devuelto con éxito")
+            else:
+                print("ERROR. El libro YA está en la biblioteca")
+        else:
+            print("ERROR. Nombre de libro incorrecto")
+        
+        
     def libroMasPrestado(self):
+        
+        max = 0
+        pos = 0
+        
+        for i in range(0, self.__TAMAÑOMAX):
+            if (self.__prestado[i] >= max):
+                max = self.__prestado[i]
+                pos = i
+        
+        return int(pos)
